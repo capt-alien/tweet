d5f7ec15bc5f558e7fe697f0cf603069d7aec162
capt-alien <eric.botcher@presidio.edu>
2018-11-08 13:28:23 -0800

    tuple works

--- histogram.py
+++ histogram.py
@@ -17,9 +17,9 @@ def unique_list(words):
     # return unique words lists
     return unique_words
 
-
 ## Create a dictionary with counts
 def dict_words(words):
+    #takes a list argument and returns a word count
     dict = {}
     for word in words:
         if word not in (dict):
@@ -28,18 +28,38 @@ def dict_words(words):
     return dict
 
 ## create a list of lists with counts
+def list_words(words):
+    #takes a list argument and returns a word count
+
+    words_list = []
+    for word in words:
+        if word not in words_list:
+            words_list.append([word, 0])
+        for item in words_list:
+            if item[0] == word:
+                item[1] += 1
+    print(words_list)
+    return words_list
 
 ##Create a list of tuples
+def tuple(words):
+    #takes a list argument and returns a word count
+    tuple_list = []
+    for word in words:
+        if (word, words.count(word)) not in tuple_list:
+            tuple_list.append((word, words.count(word)))
+    return tuple_list
 
-##create
-
-#tests
 
 #opens file_name
 file1 = argv[1]
 print_list = text_list(file1)
 type = unique_list(print_list)
 hist = dict_words(print_list)
+wlist = list_words(print_list)
+tlist = tuple(print_list)
 print(print_list)
 print(type)
 print(hist)
+print(wlist)
+print(tlist)
