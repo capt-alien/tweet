dc31efb88e46b84749e965fe652c4e672f7483de
capt-alien <eric.botcher@presidio.edu>
2019-01-21 20:33:55 -0800

    added html template

--- app.py
+++ app.py
@@ -1,23 +1,25 @@
-from flask import Flask
+from flask import Flask, render_template
 import random
 import cleanup
 import markov
 
-
-# connect to the web
+# connect to the webwith unique name
 app = Flask(__name__)
 
 
 @app.route('/')
-def result():
+def main():
     #process and import file
     words = cleanup.text_list('text/dickens.txt')
     m_chain = markov.order_mchain(2, words)
     c_start = markov.start_token(m_chain)
     walk_the_dog = markov.walk(c_start, m_chain)
-    home = markov.finalize(walk_the_dog)
+    almost = markov.finalize(walk_the_dog)
+    home = str(almost)
+    print(home)
     # # #create resulting object
-    return home
+    # return home
+    return render_template('main.html', sentance = home)
 
 
 
